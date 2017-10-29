c7d1c1de496791d4050ba457f24f5a054ca0601b
Salama Balekage <aksalj@aksalj.com>
2017-10-29 12:42:11 +0100

    Fix token request on service initialization

--- client/src/main/java/com/africastalking/services/Service.java
+++ client/src/main/java/com/africastalking/services/Service.java
@@ -102,7 +102,9 @@ public abstract class Service {
                 .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setLenient().create())) // switched from ScalarsConverterFactory
                 .client(httpClient.build());
 
-        token = fetchToken(HOST, PORT);
+        if (token == null || token.getExpiration() < System.currentTimeMillis()) {
+            token = fetchToken(HOST, PORT);
+        }
         isSandbox = token.getEnvironment().toLowerCase().contentEquals("sandbox");
         username = token.getUsername();
         
